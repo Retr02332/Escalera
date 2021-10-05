65d1292108b7347a5a9baefca53f13147d684222
Retr02332 <retr02332bughunter@gmail.com>
2021-10-04 20:32:51 -0500

    Matriz OK, movement so

--- src/Escalera/MatrizMaker.java
+++ src/Escalera/MatrizMaker.java
@@ -29,15 +29,39 @@ public class MatrizMaker extends JPanel {
     }
 
     private void fillMatriz(final int currentPos) {
-        int count = 100;
+    	int count = 1;
+        int count2 = 20;
+        int count3 = 40;
+        int count4 = 60;
+        int count5 = 80;
+        int count6 = 100;
+        
         for(int f=0; f<filas; f++) {
             for(int c=0; c<columnas; c++) {
-                final JPanel panel = centerJLabel(new JLabel(String.valueOf(count)));
+            	final JLabel jLabel = new JLabel(String.valueOf(count));
+                final JPanel panel = centerJLabel(jLabel);
+                
+                if(f == 1) {
+                    jLabel.setText(String.valueOf(count2--));
+                     panel.setBackground(Color.lightGray.brighter());
+                 } else if(f == 3) {
+                     jLabel.setText(String.valueOf(count3--));
+                     panel.setBackground(Color.lightGray.brighter());
+                 } else if(f == 5) {
+                     jLabel.setText(String.valueOf(count4--));
+                     panel.setBackground(Color.lightGray.brighter());
+                 } else if(f == 7) {
+                     jLabel.setText(String.valueOf(count5--));
+                     panel.setBackground(Color.lightGray.brighter());
+                 } else if(f == 9) {
+                     jLabel.setText(String.valueOf(count6--));
+                     panel.setBackground(Color.lightGray.brighter());
+                 }
                 if(count == currentPos) {
                 	panel.setBackground(Color.blue);
                 }
-                count--;
-                super.add(panel);
+                count++;
+                super.add(panel, c);
             }
         }
     }
